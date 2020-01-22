5d0b51a06e7d6be05f0bfd53e02d9e4c0726dc6a
thematius <thematius.monsalve@gmail.com>
2020-01-22 16:04:29 -0500

    Realizando una petición a una API usando XMLHttpRequest

--- src/callback/challenge1.js
+++ src/callback/challenge1.js
@@ -1,4 +1,5 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API_URL = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url, callback) {
     let xhttp = new XMLHttpRequest();
@@ -11,8 +12,23 @@ function fetchData(url, callback) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error =  new Error(`Error: ${url}`);
-                callback(error);
+                return callback(error);
             }
         }
     }
+    xhttp.send();
 }
+
+// Callback hell
+fetchData(API_URL, (err1, data1) => {
+    if (err1) return console.error(err1);
+    fetchData(API_URL + data1.results[0].id, (err2, data2) => {
+        if (err2) return console.error(err2);
+        fetchData(data2.origin.url, (err3, data3) => {
+            if (err3) return console.error(err3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
